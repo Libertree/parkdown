0586149491d504503382f1e40776f50bbf345dc4
rekado <rekado@elephly.net>
2012-10-27 20:32:47 +0800

    flip EXT_AUTOLINK bit when :autolink is provided

--- ext/markdown.c
+++ ext/markdown.c
@@ -23,6 +23,8 @@ rb_markdown_to_html(int argc, VALUE *argv, VALUE self)
         extensions = extensions | EXT_FILTER_STYLES ;
     if ( rb_funcall(self, rb_intern("strike"), 0) == Qtrue )
         extensions = extensions | EXT_STRIKE ;
+    if ( rb_funcall(self, rb_intern("autolink"), 0) == Qtrue )
+        extensions = extensions | EXT_AUTOLINK ;
 
     char *html = markdown_to_string(ptext, extensions, HTML_FORMAT);
     VALUE result = rb_str_new2(html);
